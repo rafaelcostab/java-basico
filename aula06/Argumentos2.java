f0da7182ac7a61e0aaf4be4feb8384fe5f8e031e
Rafael Costa <rafael.costab@hotmail.com>
2020-08-05 00:43:48 -0300

    Começando com java: Passando argumentos para o programa

---
+++ aula06/Argumentos2.java
@@ -0,0 +1,10 @@
+/**1. Criar um arquivo chamado Argumentos2.java
+ * 2. Implementar um programa Java que escreva na tela "Olá mundo!" + argumento passado para o programa
+ * 3. Compilar e Executar o programa
+ */
+
+public class Argumentos2 {
+    public static void main (String [] args){
+        System.out.println("Olá Mundo! " + args[0]);
+    }
+}
