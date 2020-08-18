9e5af85f92471afa9c5a29060fe03bb4cf4015b1
Rafael Costa <rafael.costab@hotmail.com>
2020-08-17 23:15:00 -0300

    Programação básica: Introdução a variáveis

---
+++ com/loiane/training/aula10/Variaveis.java
@@ -0,0 +1,32 @@
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+
+		//convencao java (camel case + case sensitive)
+		int idade = 25;
+		String meuNome = "Rafael";
+		String nomeDoMeuGato = "Gatolipo";
+		String ano = "2020";
+
+
+		//aceito mas nao utilizado
+		int _idade;
+		int $idade;
+
+		//nao e convencao java
+		String nome_do_meu_gato;
+		String NomeDoMeuGato;
+		String nomeDoMeugato;
+
+		idade = 26;
+
+		System.out.println("Nome: " + meuNome);
+		System.out.println("Idade: " + idade);
+
+		//ma pratica
+		int a = 10;
+		String b = "Rafael";
+	}
+
+}
