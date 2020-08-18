ee09c698c2af4d547ea9cc8c68cf7d41581f9fbc
Rafael Costa <rafael.costab@hotmail.com>
2020-08-17 22:23:09 -0300

    Começando com Java: Entendendo os Erros

---
+++ aula07/Erro03.java
@@ -0,0 +1,9 @@
+public class Erro03 {
+    public static void Main(String[] args) {
+        System.out.println("Você digitou: " + args[0]);
+    }
+
+    public static void main(String[] args) {
+        Main(args);
+    }
+}
